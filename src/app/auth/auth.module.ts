a889ea0b1dc5a16b875c5898cfa8b109206352c9
LSiccha <gabrielluis_2001@hotmail.com>
2022-10-05 18:29:06 -0500

    project structure

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }

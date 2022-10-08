c0314242b803d7ea01703178011d8a1cffc15543
LSiccha <gabrielluis_2001@hotmail.com>
2022-10-07 20:18:40 -0500

    auth service

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../core/services/auth.service";
+import {LoginCreds} from "../../../core/models/login-creds.model";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(creds: LoginCreds) {
+    this.authService.login(creds).subscribe(res => {
+      this.authService.saveUser(res);
+    })
   }
 
 }

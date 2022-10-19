410acded6976d8bb97363c1873dc264cc04553ae
LSiccha <gabrielluis_2001@hotmail.com>
2022-10-18 20:57:41 -0500

    mock home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SampleComponent} from "./pages/sample/sample.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SampleComponent } from './pages/sample/sample.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SampleComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule

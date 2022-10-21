3f92e4c75e1ed16ed5b8561cab5c6d0250f0697b
LSiccha <gabrielluis_2001@hotmail.com>
2022-10-21 14:51:05 -0500

    export mat modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './core/components/navbar/navbar.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {AuthService} from "./core/services/auth.service";
-import { MyButtonComponent } from './ui/components/my-button/my-button.component';
-import { LayoutComponent } from './core/layout/layout.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {LayoutComponent} from './core/layout/layout.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        LayoutComponent,
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule
-    ],
-    providers: [
-        HttpClient,
-        AuthService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LayoutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers: [
+    HttpClient,
+    AuthService
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MyButtonComponent} from "./components/my-button/my-button.component";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
@@ -12,7 +15,10 @@ import {MyButtonComponent} from "./components/my-button/my-button.component";
     CommonModule
   ],
   exports: [
-    MyButtonComponent
+    MyButtonComponent,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class UiModule { }
